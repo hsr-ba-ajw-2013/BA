204e29cbb0bd7a4b71e42b8be6c6be5b2875ac65
Michael Weibel <michael.weibel@gmail.com>
2013-03-08 07:21:10 +0100

    Some simple ajaxified stuff

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -5,13 +5,22 @@
 var TaskController = {
 	get: function(req, res) {
 		var id = req.param('id');
-
 		Task.find(id).done(function(err, task) {
 			User.find(task.userId).done(function(err, user) {
-				res.json({
+				var response = {
 					'task': task,
-					'user': user
-				});
+					'user': user,
+					'title': task.name
+				};
+
+				if (req.acceptJson) {
+					res.json(response);
+				} else if(req.isAjax && req.param('partial')) {
+					response['layout'] = false;
+					res.view(response);
+				} else {
+					res.view(response);
+				}
 			});
 		});
 	}
