4729a3dc47ad03a63acea0f32e05acf2adeadee6
Michael Weibel <michael.weibel@gmail.com>
2013-03-10 17:13:14 +0100

    Testing which doesn't work

---
+++ test/TaskControllerTest.js
@@ -0,0 +1,24 @@
+/**
+ * Task Controller Test
+ */
+
+// var request = require('supertest'),
+// 	assert = require('should'),
+// 	sails = require('sails');
+
+// var app = sails.lift();
+
+// describe('TaskController', function() {
+// 	describe('index', function() {
+// 		it('should have Content-Type JSON', function() {
+// 			request(app)
+// 				.get('/task')
+// 				.expect('Content-Type', /json/)
+// 				.end(function(err, res) {
+// 					if(err) {
+// 						throw err;
+// 					}
+// 				});
+// 		});
+// 	});
+// });
