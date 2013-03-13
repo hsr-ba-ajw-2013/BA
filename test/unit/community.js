5973a5b96791c5a19eb1b3fbe7aae201d9f999c8
Michael Weibel <michael.weibel@gmail.com>
2013-03-13 16:50:38 +0100

    Fix asserts to be 'should'

--- test/unit/community.js
+++ test/unit/community.js
@@ -1,19 +1,21 @@
-var should = require('chai').should
-	, expect = require('chai').expect
-	, assert = require('chai').assert
+var chai = require('chai')
+	, expect = chai.expect
 	, config = require('../../config_test')
 	, db = require('../../lib/db');
+
+	chai.should();
 
 	var sequelize = db(config);
 	describe('Get all communities', function(){
 		// yes, I know - it doesn't make any sense this test. - POC
 		it('should not have any communities', function(done){
-			sequelize.daoFactoryManager.getDAO('Community').all().success(function(communities) {
-				assert.lengthOf(communities, 0);
-				done();
-			}).error(function(error) {
-				assert.equal(error, null);
-				done();
-			});
+			(function() {
+				sequelize.daoFactoryManager.getDAO('Community').all().success(function(communities) {
+					communities.should.have.length(0);
+					done();
+				}).error(function(error) {
+					throw(error);
+				})
+			}).should.not.throw();
 		});
 	});
