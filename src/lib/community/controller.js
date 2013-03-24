ea124c6c4a5d722cf6ec58d2d2f46521e3c2fb36
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-03-24 20:00:09 +0100

    routing to 'GET create' and 'POST create' created (tbf)

--- src/lib/community/controller.js
+++ src/lib/community/controller.js
@@ -12,7 +12,9 @@ var PREFIX = '/community'
 module.exports = function(app) {
 	app.all(PREFIX + '*', loginRequired);
 	app.get(PREFIX, index);
+	//app.get(PREFIX + '/:id', showCommunity);
 	app.get(PREFIX + '/create', create);
+	app.post(PREFIX + '/create', createPost);
 }
 
 var index = function(req, res) {
@@ -31,5 +33,38 @@ var index = function(req, res) {
 };
 
 var create = function(req, res) {
-	res.send(200);
-}
+	res.render('community/views/create', { title: 'Create Community', flash: req.flash()});
+};
+
+var createPost = function(req, res) {
+	var resident = req.user
+		, Resident = req.app.get('db').daoFactoryManager.getDAO('Resident')
+		, Community = req.app.get('db').daoFactoryManager.getDAO('Community');
+
+	//TODO: validate POST
+
+	var communityData = {
+			name: req.body.cname
+		};
+
+	console.log(communityData);
+
+	Community.find({ where: communityData })
+		.success(function(community) {
+			if (community !== null) {
+				req.flash('error', "OH NO!!!");
+				res.redirect('back');
+			} else {
+				Community.create(communityData)
+					.success(function(community) {
+						console.log("community created: ", community);
+					}).error(function(errors) {
+						console.log("errors: ", errors);
+					});
+			}
+		});
+};
+
+
+/* * * * * * P R I V A T E * * * * * * * */
+
