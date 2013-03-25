4fec6c853688fc33b3ca28be8bb64a1c05b66bee
Michael Weibel <michael.weibel@gmail.com>
2013-03-25 07:30:28 +0100

    Spawn precompile-sass before starting the app

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// doesn't work yet.
-/*if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 	var spawn = require('child_process').spawn;
 
 	// before running the app, compile scss
-	spawn('make precompile-scss');
-}*/
+	spawn('make', ['precompile-sass']);
+}
 
 module.exports = process.env.COVERAGE
 	? require('./test-cov/app.js')
