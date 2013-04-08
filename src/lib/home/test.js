c3afccc4667419e41db8e80b26abda17c9e4bd46
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-04-08 19:17:47 +0200

    unit test structure for home defined

---
+++ src/lib/home/test.js
@@ -0,0 +1,72 @@
+/* global describe, it, beforeEach */
+var request = require('supertest')
+	, superagent = require('superagent')
+	, path = require('path')
+	, app = require(path.join(process.cwd(), 'index.js'))()
+	, crypto = require('crypto')
+	, doLogin = require(path.join(
+			process.cwd(), 'src', 'shared', 'test', 'passport-mock')
+		).doLogin;
+
+describe('Home', function() {
+
+	describe('GET /invite/:shareLink', function() {
+		describe('unauthorized', function() {
+			describe('with an invalid shareLink', function(){
+				it('should redirect to /', function(done){
+					request(app)
+						.get('/invite/' +
+							crypto.pseudoRandomBytes(42).toString('hex'))
+						.expect(302)
+						.expect('Location', '/', done);
+				});
+			});
+
+			describe('with a valid shareLink'
+				, function() {
+				var agent = superagent.agent();
+
+				beforeEach(function before(done) {
+					doLogin(app, agent, done);
+				});
+
+				it.skip('should redirect to ' + '/community/:slug/resident/new',
+					function() {
+
+				});
+			});
+		});
+
+		describe('authorized', function() {
+			describe('with a community for the user', function() {
+				describe('with an invalid shareLink', function() {
+
+					it.skip('should redirect to /', function(){
+
+					});
+				});
+
+				describe('with a valid shareLink', function() {
+					it.skip('should redirect to /', function(){
+
+					});
+				});
+			});
+
+			describe('without a community for the user', function() {
+				describe('with an invalid shareLink', function() {
+					it.skip('should redirect to /', function(){
+
+					});
+				});
+
+				describe('with a valid shareLink', function() {
+					it.skip('should redirect to ' +
+						'/community/:slug/resident/new', function(){
+
+					});
+				});
+			});
+		});
+	});
+});
