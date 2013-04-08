504c419e7a7456d29a2de5d81cd69dc3d08e6910
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-04-08 19:18:39 +0200

    handle GET /invite/:slug

--- src/lib/home/controller.js
+++ src/lib/home/controller.js
@@ -9,6 +9,21 @@ exports.index = function index(req, res) {
 };
 
 exports.invite = function(req, res) {
-	req = req;//FIXME REMOVE !! JSHINT IN YA FACE.
-	res.send(404);
+	var shareLink = req.params.sharelink
+		, Community = req.app.get('db').daoFactoryManager.getDAO('Community');
+
+	Community.find({ where: {shareLink: shareLink}})
+		.success(function findResult(community) {
+
+			if (community !== null) {
+				return res.redirect('/community/' +
+					community.slug + '/resident/new');
+			} else {
+				req.flash('error', res.__('This invitation link is invalid.'));
+				return res.redirect('/');
+			}
+		})
+		.error(function createError() {
+			return res.send(500);
+		});
 };
