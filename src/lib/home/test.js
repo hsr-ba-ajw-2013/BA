03d88d9b6aafc063bb236bff08e55a52be944bfb
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-04-09 23:50:14 +0200

    completed the simple unit tests

--- src/lib/home/test.js
+++ src/lib/home/test.js
@@ -24,43 +24,86 @@ describe('Home', function() {
 
 			describe('with a valid shareLink'
 				, function() {
-				var agent = superagent.agent();
 
-				beforeEach(function before(done) {
-					doLogin(app, agent, done);
-				});
-
-				it.skip('should redirect to ' + '/community/:slug/resident/new',
-					function() {
-
+				it('should redirect to /login', function(done){
+					request(app)
+						.get('/invite/' +
+							crypto.pseudoRandomBytes(42).toString('hex'))
+						.expect(302)
+						.expect('Location', '/login', done);
 				});
 			});
 		});
 
 		describe('authorized', function() {
+
+			var agent = superagent.agent();
+
 			describe('with a community for the user', function() {
+
+				beforeEach(function before(done) {
+					doLogin(app, agent, function afterLogin() {
+						var communityName =
+							crypto.pseudoRandomBytes(6).toString('hex')
+							, req = request(app)
+								.post('/community');
+
+						agent.attachCookies(req);
+						req.send({name: communityName})
+							.end(function(err) {
+								if (err) {
+									return done(err);
+								}
+								return done();
+							});
+					});
+				});
+
 				describe('with an invalid shareLink', function() {
 
-					it.skip('should redirect to /', function(){
+					it('should redirect to /', function(done){
+						var req = request(app).get('/invite/' +
+								crypto.pseudoRandomBytes(42).toString('hex'));
+						agent.attachCookies(req);
 
+						req.expect(302)
+							.expect('Location', '/', done);
 					});
 				});
 
 				describe('with a valid shareLink', function() {
-					it.skip('should redirect to /', function(){
 
+					it('should redirect to /', function(done){
+						var req = request(app).get('/invite/' +
+								crypto.pseudoRandomBytes(42).toString('hex'));
+						agent.attachCookies(req);
+
+						req.expect(302)
+							.expect('Location', '/', done);
 					});
 				});
 			});
 
 			describe('without a community for the user', function() {
-				describe('with an invalid shareLink', function() {
-					it.skip('should redirect to /', function(){
 
+				beforeEach(function before(done) {
+					doLogin(app, agent, done);
+				});
+
+				describe('with an invalid shareLink', function() {
+
+					it('should redirect to /', function(done){
+						var req = request(app).get('/invite/' +
+								crypto.pseudoRandomBytes(42).toString('hex'));
+						agent.attachCookies(req);
+
+						req.expect(302)
+							.expect('Location', '/', done);
 					});
 				});
 
 				describe('with a valid shareLink', function() {
+
 					it.skip('should redirect to ' +
 						'/community/:slug/resident/new', function(){
 
