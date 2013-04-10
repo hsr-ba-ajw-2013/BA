60ab435bf5d7515eac7b41f8a246620bc6c249d9
Michael Weibel <michael.weibel@gmail.com>
2013-04-10 10:40:41 +0200

    Update migration

--- migrations/20130408063728-0.0.2-to-0.0.3.js
+++ migrations/20130408063728-0.0.2-to-0.0.3.js
@@ -1,14 +1,67 @@
+
+function addShareLink(migration, DataTypes, done) {
+	migration.addColumn('Communities', 'shareLink', {
+		type: DataTypes.STRING, unique: true
+	}).complete(done);
+};
+
+function addSlug(migration, DataTypes, done) {
+	migration.addColumn('Communities', 'slug', {
+		type: DataTypes.STRING, unique: true
+	}).complete(done);
+}
+
+function updateCommunity(db, community, done) {
+	var path = require('path')
+		, communityController = require(path.join(__dirname, '..', 'src', 'lib', 'community', 'controller'));
+	// if there's no slug, we can assume there's no shareLink either.
+	if (community.slug === null) {
+		communityController.addUniqueSlug(db, community, function(err) {
+			if(err) {
+				return done(err);
+			}
+			communityController.createUniqueShareLink(db, function(err, link) {
+				if(err) {
+					return done(err);
+				}
+				community.shareLink = link;
+				community.save();
+				done();
+			});
+		});
+	}
+}
+
+function migrateExistingData(migration, DataTypes, done) {
+	var db = migration.migrator.sequelize
+		, crypto = require('crypto')
+		, uslug = require('uslug')
+		, path = require('path')
+		, communityModel = require(path.join(__dirname, '..', 'src', 'lib', 'community', 'model'))(null, db);
+
+	db.daoFactoryManager.getDAO('Community').findAll()
+		.success(function(communities) {
+			if (communities !== null) {
+				communities.forEach(function(community) {
+					updateCommunity(db, community, done);
+				})
+			}
+		})
+		.error(function(err) {
+			done(err);
+		});
+}
+
 module.exports = {
-  up: function(migration, DataTypes) {
-    migration.addColumn('Communities', 'shareLink', {
-    	type: DataTypes.STRING, unique: true
-    });
-    migration.addColumn('Communities', 'slug', {
-    	type: DataTypes.STRING, unique: true
-    });
-  },
-  down: function(migration) {
-    migration.removeColumn('Communities', 'shareLink');
-    migration.removeColumn('Communities', 'slug');
-  }
+	up: function(migration, DataTypes, done) {
+		addShareLink(migration, DataTypes,
+			addSlug.bind(this, migration, DataTypes,
+				migrateExistingData.bind(this, migration, DataTypes, done)
+			)
+		);
+	},
+	down: function(migration) {
+		migration.removeColumn('Communities', 'shareLink');
+		migration.removeColumn('Communities', 'slug');
+	}
 }
