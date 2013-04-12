f408c7068cca2f91edc78103f1ebf73249a07870
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-04-12 08:32:38 +0200

    log removed

--- src/lib/community/transporter.js
+++ src/lib/community/transporter.js
@@ -2,14 +2,13 @@ module.exports = function communityTransporter(req, res, next) {
 	var Community = req.app.get('db').daoFactoryManager.getDAO('Community')
 		, resident = req.user;
 
-	res.community = undefined;
-console.log("da");
+	res.locals.community = undefined;
+
 	if (!resident || !resident.CommunityId) {
 		return next();
 	} else {
 		Community.find(resident.CommunityId)
 			.success( function findResult(community) {
-				console.log(community);
 				res.locals.community = community;
 				return next();
 			})
