ff5f6e08d3f9fdee2716e76784cdc99008728abb
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-04-14 15:14:55 +0200

    add task view init

--- src/lib/task/controller.js
+++ src/lib/task/controller.js
@@ -2,6 +2,11 @@
  * Task Controller
  */
 
+var path = require('path')
+	, validatorsPath = path.join('..', '..', 'shared', 'validators')
+	, createTaskValidator = require(
+		path.join(validatorsPath, 'create-task'));
+
 exports.index = function index(req, res) {
 	var resident = req.user;
 
@@ -20,7 +25,29 @@ exports.index = function index(req, res) {
 	});
 };
 
-exports.create = exports.fresh = exports.get = exports.update =
+exports.fresh = function fresh(req, res) {
+	res.render('task/views/fresh', {
+				title: res.__('New Task')
+			});
+};
+
+var createTask = function createCommunity(req, res) {
+	var resident = req.user;
+
+	resident.getCommunity().success(function result(community) {
+
+		if (!community) {
+			return res.redirect('/');
+		}
+
+		res.flash('success', 'Task successfully added.');
+		res.redirect('..');
+	});
+};
+
+exports.create = [createTaskValidator, createTask];
+
+exports.create  = exports.get = exports.update =
 	exports.del = function(req, res) {
 		req = req;//FIXME REMOVE !! JSHINT IN YA FACE.
 		res.send(404);
---
+++ src/shared/validators/create-task.js
@@ -0,0 +1,30 @@
+/** Module: Shared.Validators.createTaskValidator
+ * Create task validator
+ */
+
+module.exports = function createTaskValidator(req, res, next) {
+	var error = res.__('Name needs to be within 1 and 255 chars.');
+	req.assert('txtTask', error).len(1, 255);
+	req.sanitize('txtTask').xss();
+	req.sanitize('txtTask').trim();
+
+	error = res.__('Reward needs to be a number between 1 and 5.');
+	req.assert('txtReward', error).len(1, 1).isInt();
+	req.sanitize('txtReward').trim();
+
+	error = res.__('Due date needs to be a date in the future.');
+	req.assert('txtDueDate', error).len(1, 1).isInt();
+	req.sanitize('txtDueDate').trim();
+
+	error = res.__('Description needs to be within 0 and 255 chars.');
+	req.assert('txtDescription', error).len(0, 255);
+	req.sanitize('txtDescription').xss();
+	req.sanitize('txtDescription').trim();
+
+	var errors = req.validationErrors();
+	if(errors) {
+		req.flash('error', errors[0].msg);
+		return res.redirect('./new');
+	}
+	return next();
+};
