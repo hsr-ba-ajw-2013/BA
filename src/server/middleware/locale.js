acc7eefb5535d53478b41162adb7582474d4a3ac
Manuel Alabor <msites@msites.net>
2013-05-05 16:24:56 +0200

    documentation for locale middleware

--- src/server/middleware/locale.js
+++ src/server/middleware/locale.js
@@ -1,8 +1,22 @@
+/** Module: Locale
+ * The Locale middleware makes use of the
+ * <locale at https://github.com/jed/locale> Express.JS middleware to identify
+ * the best fitting language when processing a request.
+ *
+ */
 var _ = require('underscore')
 	, templateLocales = require('../../shared/locales')
 	, locale = require('locale')(templateLocales.supported);
 
-	
+/** PrivateFunction: ensureLocale
+ * Ensures that a locale-cookie is present at any time when processing a
+ * request.
+ *
+ * Parameters:
+ *     (Request) req - Express.JS request object
+ *     (Response) res - Express.JS response object
+ *     (Function) next - Proceed with the next middleware callback
+ */	
 function ensureLocale(req, res, next) {
 	if(_.isUndefined(req.cookies)) {
 		req.cookies = { locale: req.locale };
@@ -17,11 +31,27 @@ function ensureLocale(req, res, next) {
 	next();
 }
 
+/** PrivateFunction: putLocaleToRequest
+ * Saves the locale value from the cookie into the request-object for later
+ * access.
+ *
+ * Parameters:
+ *     (Request) req - Express.JS request object
+ *     (Response) res - Express.JS response object
+ *     (Function) next - Proceed with the next middleware callback
+ */
 function putLocaleToRequest(req, res, next) {
 	res.cookie('locale', req.locale);
 	next();
 }
 
+/** Function: setupLocale
+ * Boots the locale middleware and adds the needed stuff to the given Express.JS
+ * applications.
+ *
+ * Parameters:
+ *     (Object) app - Express.JS app
+ */
 function setupLocale(app) {
 	app.use(locale);
 
