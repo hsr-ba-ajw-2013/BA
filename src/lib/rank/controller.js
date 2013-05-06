ae569171bff624a700e12bba325b6960eb67ea4e
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-05-06 16:10:19 +0200

    query using the sequelize methods
    query output is not flat anymore

--- src/lib/rank/controller.js
+++ src/lib/rank/controller.js
@@ -20,18 +20,24 @@ exports.index = function index(req, res) {
 			return res.redirect('..');
 		}
 
-		db.query(
-			'SELECT DISTINCT ' +
-				'SUM(t.reward) AS points ' +
-				', r."facebookId" ' +
-				', r."name" ' +
-			'FROM "Tasks" t, "Residents" r ' +
-			'WHERE t."fulfillorId" = r."id" ' +
-				'AND t."fulfilledAt" >= ' +
-					'(current_date - interval \'7 days\') ' +
-			'GROUP BY t."fulfillorId", r."id" ' +
-			'ORDER BY points DESC'
-		)
+		var residentIds = residents.map(function getResidentIds(curResident) {
+			return curResident.id;
+		});
+
+		var Task = db.daoFactoryManager.getDAO('Task')
+			, Resident = db.daoFactoryManager.getDAO('Resident')
+			, today = new Date()
+			, lastWeek = new Date(today.getTime()-1000*60*60*24*7);
+
+		Task.findAll({
+			attributes: [['SUM("reward")', 'points'], 'fulfillorId']
+			, include: [{ model: Resident, as: 'Fulfillor' }]
+			, where:
+				['"Tasks"."fulfillorId" IN (' + residentIds.join(',') + ') ' +
+					'AND "Tasks"."fulfilledAt" >= ?', lastWeek]
+			, group: ['"Tasks"."fulfillorId"', 'Fulfillor.id']
+			, order: '"points" DESC'
+		})
 		.success(function(ranks) {
 			res.render('rank/views/index', {
 				title: res.__('Tasks')
