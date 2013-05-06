63af50d3c09f813cde4cd6260950f98c2b5af522
Manuel Alabor <msites@msites.net>
2013-05-06 21:40:49 +0200

    some documentation for the api route helper

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,5 +1,16 @@
+/** Module: API
+ */
 var _ = require('underscore');
 
+/** API Routes Helper
+ * A small helper to gather API routes from submodules.
+ *
+ * It imitates the interface of express.js by providing a function for each HTTP
+ * verb. Submodules can call them to register an api route.
+ *
+ * After all routes are registered, thie API module exports the routes property
+ * as barefoot expects.
+ */
 var api = (function() {
 	return {
 		routes: {}
@@ -20,7 +31,7 @@ var api = (function() {
 })();
 
 
+// Get all the API submodules to register their routes:
 require('./resident')(api);
-
 
 module.exports = { routes: api.routes };
