ad114412025df882cfa848a58dc5893b82b96b4e
Manuel Alabor <msites@msites.net>
2013-05-15 14:55:24 +0200

    documentation for errors

--- src/server/api/community/errors.js
+++ src/server/api/community/errors.js
@@ -3,8 +3,21 @@
  */
 var Barefoot = require('barefoot')()
 	, errors = Barefoot.errors
+
+	/** Error: ResidentAlreadyInCommunityError
+	 * An error indicating that a resident is already inhabitant of a community.
+	 *
+	 * Represented by an HTTP status code 409 "Conflict"
+	 */
 	, ResidentAlreadyInCommunityError = errors.createError(409
 										, 'ResidentAlreadyInCommunityError')
+
+	/** Error: CommunityAlreadyExistsError
+	 * During creation of a community, this error indicates that that particular
+	 * community the user tries to create already exists.
+	 *
+	 * Represented by an HTTP status code 409 "Conflict"
+	 */
 	, CommunityAlreadyExistsError = errors.createError(409
 										, 'CommunityAlreadyExistsError');
 
