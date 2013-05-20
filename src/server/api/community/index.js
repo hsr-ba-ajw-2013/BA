69536059ecd322426f500cffdce252772033e4e6
Manuel Alabor <msites@msites.net>
2013-05-20 15:14:39 +0200

    cleaned up index.js for community api

--- src/server/api/community/index.js
+++ src/server/api/community/index.js
@@ -23,28 +23,33 @@ var controller = require('./controller')
 module.exports = function initCommunityApi(api, apiPrefix) {
 	var prefix = path.join(apiPrefix, modulePrefix);
 
+	// GET /api/community/:slug
 	api.get(path.join(prefix, ':slug'), [
 		basicAuthentication
 		, authorizedForCommunity
 		, controller.getCommunityWithSlug]);
 
+	// GET /api/community/:slug/tasks
 	api.get(path.join(prefix, ':slug', 'tasks'), [
 		basicAuthentication
 		, authorizedForCommunity
 		, controller.getTasksForCommunityWithSlug]);
 
+	// POST /api/community
 	api.post(prefix, [
 		basicAuthentication
 		, communityValidators.createCommunity
 		, controller.createCommunity
 		]);
 
+	// POST /community
 	api.app.post(modulePrefix, utils.buildFormRoute(
 		'success', 'error', api, [
 			communityValidators.createCommunity
 			, controller.createCommunity
 		]));
 
+	// POST /api/community/:slug/tasks
 	api.post(path.join(prefix, ':slug', 'tasks'), [
 		basicAuthentication
 		, authorizedForCommunity
@@ -52,56 +57,3 @@ module.exports = function initCommunityApi(api, apiPrefix) {
 		, controller.createTaskForCommunityWithSlug
 		]);
 };
-
-
-/*
-var controller = require('./controller')
-	//, model = require('./model')
-	, path = require('path')
-	//, loginRequired = require(path.join(
-	//	'..', '..', 'shared', 'policies', 'login-required')
-	//)
-	//, communityTransporter = require('./transporter')
-	, PREFIX = '/community'
-	, SLUG_PREFIX = PREFIX + '/:slug';
-
-/** Function: communityInit
- * Initializes the community component by adding
- * the controller to the available resources.
- *
- * Parameters:
- *   (Express) app - Initialized express application
- *
- * Returns:
- *   (Function) function to initialize relationships after creating all models.
- *
-module.exports = function communityInit(app) {
-
-	/**
-	 * /community/ GET -- index
-	 *				POST -- create
-	 *
-	 * /community/:slug GET -- get
-	 *					PUT -- update
-	 *					DEL -- del
-	 *
-	 * /community/:slug/invite GET -- invite
-	 *
-	 *
-	 * /community/new GET -- fresh (new is protected word)
-	 *
-
-	app.all(PREFIX + '*', loginRequired, communityTransporter);
-
-	app.get(PREFIX, controller.index);
-	app.post(PREFIX, controller.create);
-	app.get(PREFIX + '/new', controller.fresh);
-
-	app.get(SLUG_PREFIX, controller.get);
-	app.put(SLUG_PREFIX, controller.update);
-	app.get(SLUG_PREFIX + '/invite', controller.invite);
-
-	app.del(SLUG_PREFIX, controller.del);
-
-	return model(app);
-};*/
