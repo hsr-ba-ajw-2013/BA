7d95b7d625760e86f46da50eb1d343aaf5e07da0
Alexandre Joly <alexandre.joly@mekanics.ch>
2013-05-27 18:10:43 +0200

    unused comments removed

--- src/shared/views/resident/profile.js
+++ src/shared/views/resident/profile.js
@@ -3,14 +3,12 @@ var View = require('../roomiesView');
 module.exports = View.extend({
 	el: '#main'
 	, initialize: function() {
-		console.log("PROFILE: initialize");
 		var residentProfile = this.options.dataStore.get('residentProfile');
 		this.residentProfile = residentProfile;
 		residentProfile.on('sync', this.renderView.bind(this));
 	}
 
 	, beforeRender: function(resolve) {
-		console.log("PROFILE: beforeRender");
 		if(!this.residentProfile.model) {
 			this.residentProfile.fetch({success: function() {
 				resolve();
@@ -22,12 +20,10 @@ module.exports = View.extend({
 
 	, renderView: function() {
 		var residentProfile = this.options.dataStore.get('residentProfile');
-		console.log("PROFILE: renderView");
 		this.$el.html(this.templates.resident.profile(residentProfile));
 	}
 
 	, afterRender: function(resolve) {
-		console.log("PROFILE: afterRender");
 		this.setDocumentTitle(this.translate('Profile'));
 		resolve();
 	}
